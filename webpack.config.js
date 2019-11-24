b47266148e59e9680ac70a1d38ebac9866069155
@manumac86 <emmartinez86@gmail.com>
2019-11-24 06:02:09 -0300

    Babel config

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
                 test: /\.css$/,
                 use: [
                     {
--- webpack.dev.js
+++ webpack.dev.js
@@ -22,6 +22,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
                 test: /\.css$/,
                 use: [
                     'style-loader',
